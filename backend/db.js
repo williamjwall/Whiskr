1be09b3111aa7d93010e947d10766be070789054
William Wall <williamjwall@gmail.com>
2025-02-20 11:48:37 -0800

    Postgre and node/express for render config

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
-// db.js
-const { Pool } = require('pg');
+// backend/db.js
+const { Pool } = require("pg");
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  // You might need SSL config on Render's external DB:
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
